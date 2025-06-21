741f7410ba8f35e2f195583a62b204fa6d7e8417
LJZ458 <jizhongliu63@gmail.com>
2025-06-21 01:46:20 -0700

    Updated tables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./styles.css";
+import { Button, Input, Select, Space } from 'antd';
 import { Table } from "antd";
+const { Search } = Input;
+
 export default function App() {
+  const onChange = value => {
+  console.log(`selected ${value}`);
+};
+  const onSearch = value => {
+  console.log('search:', value);
+};
   const dataSource = [
     {
       key: "1",
@@ -61,6 +70,75 @@ export default function App() {
       <h1>Conversion Electron Particle Parameter Calculator</h1>
       <h2>Comparison of theorectical calculations for b2</h2>
       <Table dataSource={dataSource} columns={columns} />;
+      <div style={{ textAlign: 'left', marginBottom: 16 }}>
+      <Select
+    showSearch
+    placeholder="Select Source"
+    optionFilterProp="label"
+    onChange={onChange}
+    onSearch={onSearch}
+    options={[
+      {
+        value: 'Rose',
+        label: 'Rose',
+      },
+      {
+        value: 'Sliv',
+        label: 'Sliv',
+      },
+      {
+        value: 'Purdue',
+        label: 'Purdue',
+      },
+      {
+        value: 'Caltech',
+        label: 'Caltech',
+      },
+
+    ]}
+  />
+    <Select
+    showSearch
+    placeholder="Select Transition Type"
+    optionFilterProp="label"
+    onChange={onChange}
+    onSearch={onSearch}
+    options={[
+      {
+        value: 'E1',
+        label: 'E1',
+      },
+      {
+        value: 'E2',
+        label: 'E2',
+      },
+      {
+        value: 'M1',
+        label: 'M1',
+      },
+      {
+        value: 'M2',
+        label: 'M2',
+      },
+      {
+        value: 'E1M2',
+        label: 'E1M2',
+      },
+      {
+        value: 'M1E2',
+        label: 'M1E2',
+      },
+
+    ]}
+  />
+      <Space.Compact>
+      <Input addonBefore="Atomic Number" placeholder="Enter Proton Number" allowClear />
+       </Space.Compact>
+
+    <Space.Compact>
+      <Input addonBefore="Energy" placeholder="Enter Energy in keV" allowClear />
+    </Space.Compact>
+    </div>
     </div>
   );
 }
